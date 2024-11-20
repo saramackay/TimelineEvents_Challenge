df86f523d3880024fea0402b993eb356c72505bc
Sara MacKay <saramackay11@gmail.com>
2024-11-20 12:35:25 -0400

    Finished animation and added audio

--- Assets/Scripts/TriggerScript.cs
+++ Assets/Scripts/TriggerScript.cs
@@ -5,11 +5,35 @@ using UnityEngine.Playables;
 
 public class TriggerScript : MonoBehaviour
 {
-
+    public GameObject animatecamera;
     public PlayableDirector timeline;
+    public GameObject FPScamera;
+    public float timeincutscene;
+    bool cutsceneactivated = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        timeline.Play();
+        if (!cutsceneactivated)
+        {
+            timeline.Play();
+            animatecamera.SetActive(true);
+            FPScamera.SetActive(false);
+            cutsceneactivated = true;
+
+            StartCoroutine(returncamera());
+        }
+    }
+
+    private IEnumerator returncamera()
+    {
+        yield return new WaitForSeconds(timeincutscene);
+
+        animatecamera.SetActive(false);
+        FPScamera.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        cutsceneactivated = false;
     }
 }
